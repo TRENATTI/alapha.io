924243d0cf432bf6c59d991667b542e5e17036d7
boody <bodebi639@gmail.com>
2024-05-17 15:22:54 +0300

    Formatted main.go

--- main.go
+++ main.go
@@ -24,27 +24,32 @@ type BannedGroup struct {
 func main() {
 	opt := option.WithCredentialsFile("./config/creds.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
+
 	if err != nil {
 		log.Fatalf("Error initializing app: %v\n", err)
 	}
 
 	// Initialize the Realtime Database client
 	client, err := app.DatabaseWithURL(context.Background(), getEnv("FIREBASE_LINK"))
+
 	if err != nil {
 		log.Fatalf("Error initializing database client: %v\n", err)
 	}
 
 	homeHandler := func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("./templates/index.html")
+
 		if err != nil {
 			log.Printf("Error parsing HTML template: %v", err)
 			return
 		}
+
 		tmpl.Execute(w, nil)
 	}
 
 	blacklistHandler := func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("./templates/blacklist.html")
+
 		if err != nil {
 			log.Printf("Error parsing HTML template: %v", err)
 			return
@@ -54,6 +59,7 @@ func main() {
 
 		// Read data from the database
 		var data map[string]BannedGroup
+
 		if err := ref.Get(context.Background(), &data); err != nil {
 			log.Fatalf("Error getting value: %v\n", err)
 		}
